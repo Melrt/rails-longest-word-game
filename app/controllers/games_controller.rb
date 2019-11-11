250cfb77c41cba099c1a6aaeae76395eb88d65ca
melanie rat <RAT.melanie@gmail.com>
2019-11-11 16:33:25 +0100

    try ex

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,28 +3,25 @@ require 'json'
 
 class GamesController < ApplicationController
   def new
-    # display new random grid and form
-    # The form will be submitted (with POST) to the score action.
     @letters = []
     10.times { @letters << ('A'..'Z').to_a.sample }
   end
 
   def score
-    @word = params[:word]
+    @letters = params[:letters]
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
 
-    # if word.include?(@letters)
-    #   if english_word?
-    #     @score = "Well done"
-    #   else
-    #     @score = "not an english word"
-    #   end
-    # else
-    #   "not in the grid"
-    # end
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
   end
 
   def english_word?(word)
-    response = open(`https://wagon-dictionary.herokuapp.com/autocomplete/#{word}`)
+    response = open("https://wagon-dictionary.herokuapp.com/autocomplete/#{word}")
     json = JSON.parse(response.read)
     json['found']
   end
